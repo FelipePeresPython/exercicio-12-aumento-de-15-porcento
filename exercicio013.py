e810c968a78a2402804b7a9b2430bbaced9adc85
FelipePython <felipehypixel10@gmail.com>
2026-02-02 20:03:27 -0300

    Add files via upload

---
+++ exercicio013.py
@@ -0,0 +1,14 @@
+print("vamos aumentar 15% do salário do funcionário")
+print()
+print("escolha o salário dele")
+L=int(input("R$"))
+print()
+print()
+print()
+print()
+print("<agora que ja escolhemos o salário dele,iremos adicionar mais 15% do salário dele>")
+print()
+A=(15/100)*L
+B=L + A
+print(f"o aumento dele ficará: R${A} \n e o salário dele irá ficar:R${B}")
+print()
